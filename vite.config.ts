4954f12688808676cd2523b381b85bfbf0f940a9
Qianke Meng <2770998795@qq.com>
2026-01-14 12:54:44 +0800

    Fix vite config missing react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: '/icme2026_challenge_worldmodel/',
   server: {
     host: '0.0.0.0',
